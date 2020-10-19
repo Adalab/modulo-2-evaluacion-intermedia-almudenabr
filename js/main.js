71b59150851e315c1976fb7e46a8876c72b10b2b
Almudena <almudenablanco76@gmail.com>
2020-10-19 22:47:14 +0200

    solution intermediate exam-solve random, comparison with input and counter attempts

---
+++ js/main.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const inputNumber = document.querySelector(".js-number");
+const btn = document.querySelector(".js-btn"); // button
+const random = getRandomNumber(100); // nº aleatorio
+const clue = document.querySelector(".js-clue"); // pista
+const attempts = document.querySelector(".js-attempts"); // nº intentos
+
+//nº aleatorio generado
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+// recoge nº introducido y comparo con el random
+function showNumber() {
+  if (isFinished == false) {
+    // para que termine el contador
+    const inputValue = parseInt(inputNumber.value); // convierto la cadena de caracteres recibida a int
+
+    if (inputValue > 0 && inputValue <= 100) {
+      if (inputValue < random) {
+        clue.innerHTML = "El número es demasiado bajo";
+      } else if (inputValue > random) {
+        clue.innerHTML = "El número es demasiado alto";
+      } else if (inputValue === random) {
+        clue.innerHTML = "Has ganado campeona!!!";
+        isFinished = true;
+      }
+    } else {
+      clue.innerHTML = "Introduzca número entre 0 y 100";
+    }
+
+    counter();
+  } else {
+    clue.innerHTML =
+      "Juego finalizado, refresque la pantalla para volver a jugar";
+  }
+}
+
+//nº intentos
+let counterClick = 0; // variable que inicia en 0 y va a ir sumando y guardando los intentos que hagamos
+let isFinished = false;
+
+function counter() {
+  counterClick = counterClick + 1;
+  attempts.innerHTML = `Número de intentos: ${counterClick}`;
+  //let counterClick = 0; => no la puedo definir dentro de la función porque cada vez que la invoque el contador = 0
+}
+
+btn.addEventListener("click", showNumber); //evento con el que todo empieza
